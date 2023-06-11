4b67a3cbd99216ab163db6d3136ed1d8b2bf1270
Prashant1424 <prashant1424325@gmail.com>
2023-06-11 19:37:51 +0530

    Adding : Code for User Registration Problem

--- UserRegistrationProblem/src/main/java/org/example/Main.java
+++ UserRegistrationProblem/src/main/java/org/example/Main.java
@@ -1,7 +1,44 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        UserRegistration ur = new UserRegistration();
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter your First name : ");
+
+        String firstName = scan.next();
+
+        ur.firstName(firstName);
+
+        System.out.println("Enter your Last name : ");
+
+        String lastName = scan.next();
+
+        ur.lastName(lastName);
+
+        System.out.println("Enter your mail : ");
+
+        String email = scan.next();
+
+        ur.email(email);
+
+        System.out.println("Enter your Mobile number : ");
+
+        String mobNum = scan.next();
+
+        ur.mobileNum(mobNum);
+
+        System.out.println("Enter your password : ");
+
+        String psw = scan.next();
+
+        ur.password(psw);
+
     }
+
 }
--- UserRegistrationProblem/src/main/java/org/example/UserRegistration.java
+++ UserRegistrationProblem/src/main/java/org/example/UserRegistration.java
@@ -1,5 +1,65 @@
 package org.example;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class UserRegistration {
+
+    public void firstName(String name){
+
+        Pattern p = Pattern.compile("[A-Z]{1}[a-z]{2,}");
+
+        Matcher m = p.matcher(name);
+
+        boolean b = m.matches();
+
+        System.out.println("Your entered First name is " + (b ? "valid":"invalid") + ".");
+    }
+
+    public void lastName(String name){
+
+        Pattern p = Pattern.compile("[A-Z]{1}[a-z]{2,}");
+
+        Matcher m = p.matcher(name);
+
+        boolean b = m.matches();
+
+        System.out.println("Your entered Last name is " + (b ? "valid":"invalid") + ".");
+    }
+
+    public void email(String mail){
+
+        Pattern p = Pattern.compile("^[a-zA-Z0-9_+&*-]+(?:\\." + "[a-zA-Z0-9_+&*-]+)*@" +
+                "(?:[a-zA-Z0-9-]+\\.)+[a-z" + "A-Z]{2,7}$");
+
+        Matcher m = p.matcher(mail);
+
+        boolean b = m.matches();
+
+        System.out.println("Your entered email is " + (b ? "valid":"invalid") + ".");
+    }
+
+    public void mobileNum(String num){
+
+        Pattern p = Pattern.compile("(91){0,1}[6-9][0-9]{9}");
+
+        Matcher m = p.matcher(num);
+
+        boolean b = m.matches();
+
+        System.out.println("Your entered Mobile number is " + (b ? "valid":"invalid") + ".");
+    }
+
+    public void password(String name){
+
+        Pattern p = Pattern.compile("^.*(?=.{8,})(?=..*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=]).*$");
+
+        Matcher m = p.matcher(name);
+
+        boolean b = m.matches();
+
+        System.out.println("Your entered Password is " + (b ? "valid":"invalid") + ".");
+
+    }
 
 }
